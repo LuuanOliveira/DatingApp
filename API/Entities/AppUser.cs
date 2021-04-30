543c17511529465a2c33e550d11372b6a76da590
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-04-30 15:05:33 -0300

    extension to calculate age :coffin:

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -20,5 +20,10 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+
+        public int GetAge() 
+        {
+            return 99;
+        }
     }
 }
