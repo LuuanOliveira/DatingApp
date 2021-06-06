d0aa603cd87bbbbfafb9f0eed834efcc72475626
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-06-06 14:38:45 -0300

    :construction: adding loading indicators

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,6 +22,8 @@ import { MemberCardComponent } from './pages/members/member-card/member-card.com
 import { LoginComponent } from './pages/login/login.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './pages/members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -46,11 +48,13 @@ import { MemberEditComponent } from './pages/members/member-edit/member-edit.com
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    NgxSpinnerModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
