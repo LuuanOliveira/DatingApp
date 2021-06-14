371cafc2e9ab6a946f863e370c771f9b72f0311d
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-06-14 15:14:58 -0300

    adding reCAPTCHA to register :white_check_mark:

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -9,7 +9,7 @@ import { MemberEditComponent } from '../pages/members/member-edit/member-edit.co
 export class PreventUnsavedChangesGuard implements CanDeactivate<unknown> {
   canDeactivate(component: MemberEditComponent): boolean {
     if (component.editForm.dirty) {
-      return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
+      return confirm('Você tem certeza que quer continuar? Todas as alterações não salvas serão perdidas');
     }
     return true;
   }
--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../../_services/account.service';
 
@@ -9,21 +11,53 @@ import { AccountService } from '../../_services/account.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  captchaResponse: string;
+  captchaSuccess: boolean = false;
+  captchaIsExpired: boolean = false;
+  siteKey: string = "6LeRhSIbAAAAALKN8pyF5Y6Fd-BbI5atWFioe7gH";
+  registerFormGroup: FormGroup;
 
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  constructor(
+    private accountService: AccountService, 
+    private toastr: ToastrService,
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.registerFormGroup = this.fb.group({
+      recaptcha: ['', Validators.required],
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
   register() {
-    this.accountService.register(this.model).subscribe(response => {
-      console.log(response);
+    if (!this.captchaSuccess) {
+      this.toastr.warning("reCAPTCHA Inválido"); 
+      return
+    }
+    this.accountService.register(this.getForm()).subscribe(response => {
       this.cancel();
     }, error => {
-      console.log(error);
       this.toastr.error(error.error);
     })
+  }
+
+  getForm(): any {
+    return {
+      userName: this.registerFormGroup.get('userName').value,
+      password: this.registerFormGroup.get('password').value
+    }
+  }
+  
+  handleSuccess(captchaResponse: string): void {
+    this.captchaSuccess = true;
+    this.captchaResponse = captchaResponse;
+    this.captchaIsExpired = false;
+  }
+
+  handleError(): void {
+    this.captchaSuccess = false;
   }
 
   cancel() {
