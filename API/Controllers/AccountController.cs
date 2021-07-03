afe0091bb7d6f6c9a018a45dc9fe31ffb7a0675d
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-07-03 13:59:49 -0300

    adding the main photo to the nav bar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -57,6 +58,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
+                .Include(p => p.Photos)
                 .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
 
             if (user == null) return Unauthorized("Usuário Inválido");
@@ -73,7 +75,8 @@ namespace API.Controllers
             return new UserDto 
             {
                 UserName = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
