f9c8accd8522b571fca2207e1266a34081c0e6d2
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-08-28 20:25:22 -0300

    adding blockui :art:

--- client/src/app/_services/busy.service.ts
+++ client/src/app/_services/busy.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusyService {
+  @BlockUI() blockUI: NgBlockUI;
   busyRequestCount = 0;
 
   constructor(
@@ -13,18 +15,20 @@ export class BusyService {
 
   busy() {
     this.busyRequestCount++;
-    this.spinnerService.show(undefined, {
-      type: 'square-loader',
-      bdColor: 'rgba(255,255,255,0)',
-      color: '#333333'
-    });
+    this.blockUI.start('Carregando...')
+    // this.spinnerService.show(undefined, {
+    //   type: 'square-loader',
+    //   bdColor: 'rgba(255,255,255,0)',
+    //   color: '#333333'
+    // });
   }
 
   idle() {
     this.busyRequestCount--;
     if (this.busyRequestCount <= 0) {
       this.busyRequestCount = 0;
-      this.spinnerService.hide();
+      this.blockUI.reset();
+      // this.spinnerService.hide();
     }
   }
 }
