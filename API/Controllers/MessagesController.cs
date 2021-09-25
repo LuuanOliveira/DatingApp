265e21111c66fc8f8edfcb56b5b591defadeaf3f
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-09-25 13:23:36 -0300

    adding a message controller

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,59 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+
+        public MessagesController(IUserRepository userRepository, 
+            IMessageRepository messageRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _messageRepository = messageRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto) 
+        {
+            var username = User.GetUserName();
+
+            if (username == createMessageDto.RecipientUserName.ToLower())
+                return BadRequest("Você não pode enviar mensagens para si mesmo");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username); //remetente
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUserName); //destinatario
+
+            if (recipient == null) return NotFound();
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUserName = sender.UserName,
+                RecipientUserName = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+
+            return BadRequest("Falha ao enviar a mensagem");
+        }
+    }
+}
