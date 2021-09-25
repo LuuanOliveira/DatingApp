0533b453251bc4e721bc6fef119df256437e7195
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-09-25 11:54:27 -0300

    Setting up the entities for

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -23,5 +23,7 @@ namespace API.Entities
         public ICollection<Photo> Photos { get; set; }
         public ICollection<UserLike> LikedByUsers { get; set; } //Lista usuário que me curtiram
         public ICollection<UserLike> LikedUsers { get; set; } //Lista usuários que curti
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
     }
 }
