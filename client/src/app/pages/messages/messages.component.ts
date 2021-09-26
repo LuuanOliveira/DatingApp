17e321e2c8bfd91c5dff508cb5db6118b7934c3d
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-09-26 15:24:03 -0300

    setting up the angular app for messaging

--- client/src/app/pages/messages/messages.component.ts
+++ client/src/app/pages/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from 'src/app/_models/message';
+import { Pagination } from 'src/app/_models/paginations';
+import { MessageService } from 'src/app/_services/message.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  container: 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(resp => {
+      this.messages = resp.result;
+      this.pagination = resp.pagination;
+
+    })
+  }
+
+  pageChanges(event: any) {
+    this.pageNumber = event.page;
+    this.loadMessages();
   }
 
 }
