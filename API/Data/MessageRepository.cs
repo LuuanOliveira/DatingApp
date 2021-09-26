8f75d6b11cfd52c45efd38db291ba21dc0a12d16
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-09-26 14:44:44 -0300

    getting the message thread for two users

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -4,6 +4,7 @@ using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -56,9 +57,34 @@ namespace API.Data
             return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
-        public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
+        public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUserName, 
+            string recipientUserName)
         {
-            throw new NotImplementedException();
+            var messages = await _context.Messages
+                .Include(u => u.Sender).ThenInclude(p => p.Photos) //remetente
+                .Include(u => u.Recipient).ThenInclude(p => p.Photos) //destinatario
+                .Where(m => m.Recipient.UserName == currentUserName
+                        && m.Sender.UserName == recipientUserName
+                        || m.Recipient.UserName == recipientUserName
+                        && m.Sender.UserName == currentUserName
+                 )
+                .OrderBy(m => m.MessageSent)
+                .ToListAsync();
+
+            var unreadMessages = messages.Where(m => m.DateRead == null
+                && m.RecipientUserName == currentUserName).ToList();
+
+            if (unreadMessages.Any())
+            {
+                foreach (var message in unreadMessages)
+                {
+                    message.DateRead = DateTime.Now;
+                }
+
+                await _context.SaveChangesAsync();
+            }
+
+            return _mapper.Map<IEnumerable<MessageDto>>(messages);
         }
 
         public async Task<bool> SaveAllAsync()
