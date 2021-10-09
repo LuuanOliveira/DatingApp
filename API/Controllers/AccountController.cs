1f042ef289eb2140d56031c93cc55b48b420014f
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-10-08 21:03:46 -0300

    setting up the entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,11 +33,7 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-              
             user.UserName = registerDto.UserName.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
@@ -60,15 +56,6 @@ namespace API.Controllers
 
             if (user == null) return Unauthorized("Usuário Inválido");
 
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int count = 0; count < computedHash.Length; count++)
-            {
-                if (computedHash[count] != user.PasswordHash[count]) return Unauthorized("Senha Inválida");
-            }
-            
             return new UserDto 
             {
                 UserName = user.UserName,
