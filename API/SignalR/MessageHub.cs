5c71302ad0ae7b927920b9b79afefea8e9c0977b
Luan Oliveira <victor.a.oliveira@capgemini.com>
2021-10-30 14:24:46 -0300

    :package: sending messages via the hub

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
 using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
@@ -10,11 +12,14 @@ namespace API.SignalR
     public class MessageHub : Hub
     {
         private readonly IMessageRepository _messageRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
-        public MessageHub(IMessageRepository messageRepository, IMapper mapper)
+        public MessageHub(IMessageRepository messageRepository, IMapper mapper, 
+            IUserRepository userRepository)
         {
             _mapper = mapper;
             _messageRepository = messageRepository;
+            _userRepository = userRepository;
         }
 
         public override async Task OnConnectedAsync() 
@@ -28,6 +33,36 @@ namespace API.SignalR
                 .GetMessageThread(Context.User.GetUserName(), otherUser);
 
             await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+        }
+
+        public async Task SendMessage(CreateMessageDto createMessageDto) 
+        {
+            var username = Context.User.GetUserName();
+
+            if (username == createMessageDto.RecipientUserName.ToLower())
+                throw new HubException("Você não pode enviar mensagens para si mesmo");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username); //remetente
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUserName); //destinatario
+
+            if (recipient == null) throw new HubException("Usuário não encontrado");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUserName = sender.UserName,
+                RecipientUserName = recipient.UserName,
+                Content = createMessageDto.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync()) 
+            {
+                var group = GetGroupName(sender.UserName, recipient.UserName);
+                await Clients.Group(group).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
+            }
         }
 
         public override async Task OnDisconnectedAsync(Exception exception) 
--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
@@ -33,7 +34,13 @@ export class MessageService {
 
     this.hubConnection.on('ReceiveMessageThread', messages => {
       this.messageThreadSource.next(messages);
-    })
+    });
+
+    this.hubConnection.on('NewMessage', message  => {
+      this.messageThread$.pipe(take(1)).subscribe(messages => {
+        this.messageThreadSource.next([...messages, message])
+      })
+    });
   }
 
   stopHubConnection() {
@@ -50,8 +57,9 @@ export class MessageService {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
   }
 
-  sendMessage(username: string, content: string) {
-    return this.http.post<Message>(this.baseUrl + 'messages', {recipientUserName: username, content}) //Se o nome dos parametros forem iguais não é preciso expecificar, Ex. content: content
+  async sendMessage(username: string, content: string) {
+    return this.hubConnection.invoke('SendMessage', {recipientUserName: username, content}) //Se o nome dos parametros forem iguais não é preciso expecificar, Ex. content: content
+      .catch(error => console.log(error));
   }
 
   deleteMessage(id: number) {
