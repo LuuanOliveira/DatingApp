264aa6fa9e9cecf158e5751ae192594e40828fd5
Luan Oliveira <victor.a.oliveira@capgemini.com>
2022-01-24 18:30:50 -0300

    implementing the unit of work

---
+++ API/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    interface IUnitOfWork
+    {
+        IUserRepository UserRepository {get;}
+        IMessageRepository messageRepository {get;}
+        ILikesRepository likesRepository {get;}
+        Task<bool> Complete();
+        bool HasChanges();
+    }
+}
--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -10,7 +10,6 @@ namespace API.Interfaces
     {
         void Update(AppUser user);
         void DeleteUserAsync(AppUser user);
-        Task<bool> SaveAllAsync();
         Task<IEnumerable<AppUser>> GetUsersAsync();
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByUsernameAsync(string username);
