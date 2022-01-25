7442884885e731197581f3c3748bf90b74370fef
Luan Oliveira <victor.a.oliveira@capgemini.com>
2022-01-25 08:57:32 -0300

    optimizing queries

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -81,5 +81,13 @@ namespace API.Data
         {
             _context.Entry(user).State = EntityState.Deleted;
         }
+
+        public async Task<string> GetUserGender(string username)
+        {
+            return await _context.Users
+                .Where(u => u.UserName == username)
+                .Select(u => u.Gender)
+                .FirstOrDefaultAsync();
+        }
     }
 }
